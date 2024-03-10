ed7b27f000b1943404431d12c43554e38721dc98
sgarg2023 <shubhrag2021@gmail.com>
2024-03-11 03:58:06 +0530

    todo list project

---
+++ todolist/main.py
@@ -0,0 +1,9 @@
+user_text = "enter a to-do:"
+todo1 = input(user_text)
+todo2 = input(user_text)
+todo3 = input(user_text)
+todos = [todo1, todo2, todo3]
+print(todos)
+print(type(user_text))
+print(type(todo1))
+print(type(todos))
